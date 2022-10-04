cfa3532d5f460c05b2ee8e8fe046f09539c77fc4
Ami Smith <ami.slater.26@gmail.com>
2022-10-04 10:18:46 -0600

    Text Validation Project Completed

---
+++ Projects/Personality Quiz/Personality Quiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Ami Smith on 10/3/22.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
