fa4904f5958620fe453c09f44be481c47cb2c67c
Ami Smith <ami.slater.26@gmail.com>
2022-11-02 20:29:12 -0600

    Finished Meal Tracker App

--- 2. Data Collections/Labs/Meal Tracker/Meal Tracker/FoodTableViewController.swift
+++ 2. Data Collections/Labs/Meal Tracker/Meal Tracker/FoodTableViewController.swift
@@ -9,21 +9,24 @@ import UIKit
 
 class FoodTableViewController: UITableViewController {
     
-    var meals: [Meal] = [
-        Meal(name: "Breakfast", food: [Food(name: "Scrambled Eggs", description: "Yummy, healthy eggs! A good source of protein. They go great with a piece of toast!"), Food(name: "Toast", description: "Crunchy, buttery bread.")]),
-        Meal(name: "Lunch", food: [Food(name: "Salad", description: "A nice assortment of rabbit food, displayed artfully in a bowl and best served with a hint of dressing.")]),
-        Meal(name: "Dinner", food: [Food(name: "Pizza", description: "America's favorite food")])
+    var meals: [Meal] {
         
-    ]
+        let firstBreakfastFood = Food(name: "Scrambled Eggs", description: "Yummy, healthy eggs! A good source of protein. They go great with a piece of toast!")
+        let secondBreakfastFood = Food(name: "Toast", description: "Crunchy, buttery bread.")
+        let breakfast = Meal(name: "Breakfast", food: [firstBreakfastFood, secondBreakfastFood])
+        
+        
+        let firstLunchFood = Food(name: "Salad", description: "A nice assortment of rabbit food, displayed artfully in a bowl and best served with a hint of dressing.")
+        let lunch = Meal(name: "Lunch", food: [firstLunchFood])
+        
+        let firstDinnerFood = Food(name: "Pizza", description: "America's favorite food")
+        let dinner = Meal(name: "Dinner", food: [firstDinnerFood])
+        
+        return [breakfast, lunch, dinner]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
@@ -34,63 +37,30 @@ class FoodTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return meals[section].food.count
+        let meal = meals[section]
+        let food = meal.food
+        
+        return food.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Food", for: indexPath)
         let meal = meals[indexPath.section]
+        let food = meal.food[indexPath.row]
 
-        // Configure the cell...
-
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = food.name
+        content.secondaryText = food.description
+        cell.contentConfiguration = content
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let meal = meals[section]
+        
+        return meal.name
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
